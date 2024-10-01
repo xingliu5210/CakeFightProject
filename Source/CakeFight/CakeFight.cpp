0891c80fb50f4976d1080ff8a4358ed79cc63600
xingliu5210 <liu.xing4@northeastern.edu>
2024-10-01 14:13:58 -0400

    Commit

---
+++ Source/CakeFight/CakeFight.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CakeFight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CakeFight, "CakeFight" );
+
+DEFINE_LOG_CATEGORY(LogCakeFight)
+ 
---
+++ Source/CakeFight/CakeFightGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CakeFightGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACakeFightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACakeFightGameMode();
+};
+
+
+
