51afcc30a38339fde7fb345c83102b43a837127b
xingliu5210 <liu.xing4@northeastern.edu>
2024-10-01 20:29:18 -0400

    Created C++ Player Cake
    Created PlayerCake.h and cpp

---
+++ Source/CakeFight/PlayerCake.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCake.h"
+#include "PlayerCake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAKEFIGHT_API APlayerCake : public ABaseCake
+{
+	GENERATED_BODY()
+	
+};
