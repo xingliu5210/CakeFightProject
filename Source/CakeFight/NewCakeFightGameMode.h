6050f6ce9f9ac22d75daea53248de5a208e4dacf
xingliu5210 <liu.xing4@northeastern.edu>
2024-10-03 18:31:54 -0400

    Created New GameMode
    NewGameMode

---
+++ Source/CakeFight/NewCakeFightGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewCakeFightGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAKEFIGHT_API ANewCakeFightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
