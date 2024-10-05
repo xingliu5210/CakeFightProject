05c419e9f9a57ec31eabc7dd2c68c88352acbacb
xingliu5210 <liu.xing4@northeastern.edu>
2024-10-05 13:25:29 -0400

    Added EnemyCakeTwo
    EnemyCakeTwo

---
+++ Source/CakeFight/EnemyCakeTwo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCake.h"
+#include "EnemyCakeTwo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAKEFIGHT_API AEnemyCakeTwo : public ABaseCake
+{
+	GENERATED_BODY()
+	
+};
